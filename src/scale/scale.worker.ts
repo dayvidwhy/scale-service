8b77d973a9e790eccd6dd8338fe4bd9786079732
dayvidwhy <dayvidwhy@gmail.com>
2024-05-13 17:42:09 +1000

    No implicit any.

--- src/scale/scale.worker.ts
+++ src/scale/scale.worker.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { parentPort } = require("worker_threads");
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-parentPort.on("message", async ({ jobGuid }) => {
+parentPort.on("message", async ({ jobGuid }: { jobGuid: string }) => {
     await sleep(5000);
     parentPort.postMessage({ message: "Done", jobGuid });
 });
